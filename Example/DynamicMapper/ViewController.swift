6e6ee55a6948e28c5d6f8fc80441f025ffc799e4
Abdulrahman Qasem <Abdulrahman.qasem@baaz.com>
2023-09-18 16:02:36 +0300

    test

---
+++ Example/DynamicMapper/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DynamicMapper
+//
+//  Created by Abdulrahman Qasem on 09/18/2023.
+//  Copyright (c) 2023 Abdulrahman Qasem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
