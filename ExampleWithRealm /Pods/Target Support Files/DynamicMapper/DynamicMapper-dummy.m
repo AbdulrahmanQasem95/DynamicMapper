ded92308ddf4a16b566137c0bbb665ed7d3633e7
Abdulrahman Qasem <Abdulrahman.qasem@baaz.com>
2023-10-16 08:52:40 +0300

    ignore Realm example pods

--- ExampleWithRealm /Pods/Target Support Files/DynamicMapper/DynamicMapper-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DynamicMapper : NSObject
-@end
-@implementation PodsDummy_DynamicMapper
-@end
--- ExampleWithRealm /Pods/Target Support Files/Pods-DynamicMapper_Tests/Pods-DynamicMapper_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DynamicMapper_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_DynamicMapper_Tests
-@end
